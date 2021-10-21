6781cc1ac3671bf3232464b3e5e60395436983cf
TheMaxMur <muravjev.mak@yandex.ru>
2021-10-22 01:28:46 +0300

    first commit

---
+++ encode.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+import re
+import sys
+from haffman import *
+
+G_CODE_H = {}
+
+# Create Haffman dictionary
+def create_dict(node, code=''):
+    if node:
+        if node.left == None and node.right == None:
+            G_CODE_H[node.letter] = code
+            #print(f"Частота символа %s: %d. Код: %s" % (node.letter, node.weight, code))
+        create_dict(node.left, code + '0')
+        create_dict(node.right, code + '1')
+
+# Entrypoint
+def main(text: str=''):
+    global G_CODE_H
+    G_CODE_H = {}
+    #flag = input("Code by symbol or by word? (w/s): ")
+    flag = 'w'
+    try:
+        open("dictionary", 'w')
+    except:
+        pass
+    if flag == 's':
+        root = generate(list(text))[0]
+    elif flag == 'w':
+        text = clear_txt(text).upper()
+        massive = text.split()
+        tmp = []
+        for i in range(len(massive)):
+            tmp.append(massive[i])
+            if i != len(massive) - 1:
+                tmp.append(" ")
+        massive = tmp
+        del tmp
+        root = generate(massive)[0]
+        del massive
+    else:
+        print("ERROR")
+        return
+    create_dict(root)
+    G_CODE_H = dict(sorted(G_CODE_H.items(), key=lambda x: len(x[1])))
+    '''for el in G_CODE_H.keys():
+        if el == '\n':
+            print(f"Symbol '\\n'. Code: %s." % G_CODE_H[el])
+            continue
+        if el == ' ':
+            print(f"Symbol '\\s'. Code: %s." % G_CODE_H[el])
+            continue
+        print(f"Symbol '%s'. Code: %s." % (el, G_CODE_H[el]))'''
+    if flag == 's':
+        text = encode_text(text, G_CODE_H)
+    else:
+        text = encode_text_word(text, G_CODE_H)
+   #print(f"Encoded text: %s" % text)
+    with open("encode", 'w') as encode_file:
+        encode_file.write(f"%s\n" % text)
+
+def read_file(filename: str):
+    if filename:
+        try:
+            with open(filename, 'r') as data:
+                return (data.read())#.replace('\n', ' '))
+        except:
+            pass
+
+if __name__ == '__main__':
+    #argv = sys.argv[1:]
+    argv = ["text"]
+    if argv:
+        for el in argv:
+            text = read_file(el)
+            if text:
+                main(text)
+            else:
+                print("ERROR. Can't read file.")
+
+    else:
+        main('ababasdbas bbbsafs dflk jsdaasdf')
---
+++ haffman.py
@@ -0,0 +1,105 @@
+import re
+
+# Binary tree struct
+class Node():
+    def __init__(self, letter: str, weight: int, left=None, right=None):
+        self.letter = letter
+        self.weight = weight
+        self.left = left
+        self.right = right
+
+# Sort dictionary by value
+def sort_dict(d: dict):
+    sorted_dict = {}
+    sorted_keys = sorted(d, key=d.get)
+
+    for w in sorted_keys:
+        sorted_dict[w] = d[w]
+    return (sorted_dict)
+
+# Generate dictionary by frequency
+def generate_dict(text: list=[]):
+    result = {}
+    for let in text:
+        if let not in list(result.keys()):
+            result[let] = 1
+        else:
+            result[let] += 1
+    return (result)
+
+# Generate massive objects 
+def generate_first_lvl_tree(d: dict):
+    massive = []
+    for key in d.keys():
+        massive.append(Node(key, d[key]))
+    return (massive)
+
+# Create binary tree lvl
+def generate_tree(m: list):
+    massive = []
+    letter = m[0].letter + m[1].letter
+    weight = m[0].weight + m[1].weight
+    left = m[0]
+    right = m[1]
+    massive.append(Node(letter, weight, left, right))
+    for i in range(2, len(m)):
+        massive.append(m[i])
+    massive.sort(key=lambda x: x.weight)
+    return (massive)
+
+# Generate binary tree
+def generate(text: list=[]):
+    result = sort_dict(generate_dict(text))
+    with open("dictionary", 'a') as table:
+        for el in result.keys():
+            table.write(f"'%s' : %s\n" % (el, result[el]))
+    massive = generate_first_lvl_tree(result)
+    while (len(massive) > 1):
+        massive = generate_tree(massive)
+    return (massive)
+
+# Generate binary tree for decode
+def generate_for_decode(result: dict={}):
+    massive = generate_first_lvl_tree(result)
+    while (len(massive) > 1):
+        massive = generate_tree(massive)
+    return (massive)
+
+# Encode text by Haffman code
+def encode_text(text: str='', code_h: dict={}):
+    res = ""
+    while text:
+        for k in code_h:
+            if text.startswith(k):
+                res += code_h[k]
+                text = text[len(k):]
+    #text = re.sub(' ', '', text)
+    return (res)
+
+# Decode text by Haffman code
+def decode_text(text: str='', dictionary: dict={}):
+    res = ""
+    while text:
+        for k in dictionary:
+            if text.startswith(dictionary[k]):
+                res += k
+                text = text[len(dictionary[k]):]
+    return (res)
+
+def encode_text_word(text: str='', dictionary: dict={}):
+    massive = text.split()
+    result = []
+    for i in range(len(massive)):
+        result.append(dictionary[massive[i]])
+        if i != len(massive) - 1:
+            result.append(dictionary[" "])
+    massive = ''
+    for el in result:
+        massive += el
+    return (massive)
+
+def clear_txt(text: str=''):
+    text = re.sub(r"[^A-Za-z0-9А-Яа-я\s]", "", text)
+    text = re.sub(r"\n", "", text)
+    return (text)
+
